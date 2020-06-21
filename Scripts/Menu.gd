f8bc4f74e1dc458e4053def06c51362256713673
MasterPolska123 <karoltomaszewskimusic@gmail.com>
2020-06-21 21:33:50 +0200

    Fixed errors with windows export and removed mono and all things that are using .dll files, etc.

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -2,13 +2,39 @@ extends Control
 var tekst = "Hello World"
 var world_scene
 var website
+var day = OS.get_date().day
+var month = OS.get_date().month
+onready var easteregg_imgs = [
+	'res://Graphics/NewTheFox/1.png',
+	'res://Graphics/NewTheFox/2.png',
+	'res://Graphics/NewTheFox/3.png',
+	'res://Graphics/NewTheFox/4.png',
+	'res://Graphics/NewTheFox/5.png',
+	'res://Graphics/NewTheFox/6.png',
+	'res://Graphics/NewTheFox/7.png',
+	'res://Graphics/NewTheFox/8.png',
+	'res://Graphics/NewTheFox/9.png',
+	'res://Graphics/NewTheFox/10.png',
+	'res://Graphics/NewTheFox/11.png',
+]
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if day == 21 and month == 6:
+		$IMG_0008.hide()
+		load_easterregg_animation()
+	elif day == 17 and month == 2:
+		$IMG_0008.hide()
+		load_easterregg_animation()
+	elif day == 25 and month == 3:
+		$IMG_0008.hide()
+		load_easterregg_animation()
 	BackgroundLoad.play_start_transition = true
 	get_tree().paused = false
 #	$AnimationPlayer.play('end_transition')
 	print('Game launched successfully!\n')
 
+func load_easterregg_animation():
+	$IMG_0009.texture = load(str(easteregg_imgs[randi()%easteregg_imgs.size()]))
 func _on_World1_pressed():
 	Globals.world = "res://Scenes/Maps/MainWorld.tscn"
 	if Globals.selected_character == null:
