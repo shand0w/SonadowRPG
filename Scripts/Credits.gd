7c23321aaa316a9a8b3e9781c1fbfda65ce14ab3
MasterPolska123 <karoltomaszewskimusic@gmail.com>
2020-06-27 14:13:31 +0200

    credits animation fix

--- Scripts/Credits.gd
+++ Scripts/Credits.gd
@@ -8,13 +8,13 @@ extends Control
 #var authors = null# regex.sub(',', '\n', true)
 #var godot_copyrights_1 = str(Engine.get_copyright_info())
 #var godot_copyrights = godot_copyrights_1.replace(',', '\n')
-var authors_engine = "---GODOT ENGINE DEVELOPMENT TEAM---\n\n--PROJECT MANAGERS--\n" + str(Engine.get_author_info().project_managers) + '\n\n--LEAD DEVELOPERS--\n' + str(Engine.get_author_info().lead_developers) + '\n\n--FOUNDERS--\n' + str(Engine.get_author_info().founders) + '\n\n--DEVELOPERS--\n' + str(Engine.get_author_info().developers)
+#var authors_engine = "---GODOT ENGINE DEVELOPMENT TEAM---\n\n--PROJECT MANAGERS--\n" + str(Engine.get_author_info().project_managers) + '\n\n--LEAD DEVELOPERS--\n' + str(Engine.get_author_info().lead_developers) + '\n\n--FOUNDERS--\n' + str(Engine.get_author_info().founders) + '\n\n--DEVELOPERS--\n' + str(Engine.get_author_info().developers)
 var copyright = "[center]\n\nPowered by Sonadow Engine\nSonadow RPG - Copyright 2020 - " + str(OS.get_date().year) + " - Sonadow DEV\nSonadow Engine - Copyright 2020 - " + str(OS.get_date().year) + " - Sonadow DEV[/center]"
-var authors_no_bbcode_2 = authors_engine.replace(',', '\n')
-var authors_no_bbcode_3 = authors_no_bbcode_2.replace('[', '')
-var authors_no_bbcode = authors_no_bbcode_3.replace(']', '')
+#var authors_no_bbcode_2 = authors_engine.replace(',', '\n')
+#var authors_no_bbcode_3 = authors_no_bbcode_2.replace('[', '')
+#var authors_no_bbcode = authors_no_bbcode_3.replace(']', '')
 
-var authors = '[center]' + str(authors_no_bbcode) + '[/center]'
+#var authors = '[center]' + str(authors_no_bbcode) + '[/center]'
 var text
 var file = File.new()
 # Called when the node enters the scene tree for the first time.
@@ -25,6 +25,10 @@ func _ready():
 #	$RichTextLabel.set_text(text)
 #	regex.compile(authors_engine)
 #	authors = str(regex.sub(',', '\n', true))
-	$RichTextLabel.bbcode_text += authors
+#	$RichTextLabel.bbcode_text += authors
 	$RichTextLabel.bbcode_text += copyright
 #	$RichTextLabel.bbcode_text += godot_copyrights
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	get_tree().change_scene("res://Scenes/Menu.tscn")
