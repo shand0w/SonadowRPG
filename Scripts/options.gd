20cc24068f516be214fb7bd4be015e78a271aeb8
MasterPolska123 <karoltomaszewskimusic@gmail.com>
2020-08-07 19:11:05 +0200

    added option button in options to select fmod speakermode

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -16,8 +16,10 @@ func _ready():
 	if str(OS.get_name()) == 'Android':
 #		$tabs/Sterowanie.hide()
 		$tabs.set_tab_disabled(1, true)
+		$"tabs/Ogólne/Options/Audio/SPEAKERMODE".set_disabled(true)
 		$"tabs/Ogólne/Options/Graphics/custom_resolution".hide()
 	else:
+		$"tabs/Ogólne/Options/Audio/SPEAKERMODE".set_disabled(false)
 		$tabs.set_tab_disabled(2, false)
 #		$tabs/Sterowanie.show()
 		$"tabs/Ogólne/Options/Graphics/custom_resolution".show()
@@ -35,6 +37,7 @@ func _process(_delta):
 	save_file.set_value('Audio', 'music_bus_enabled', str($"tabs/Ogólne/Options/Audio/Music/Music_on".pressed))
 	save_file.set_value('Audio', 'sfx_bus_volume', str($"tabs/Ogólne/Options/Audio/SFX/SFX_slider".value))
 	save_file.set_value('Audio', 'sfx_bus_enabled', str($"tabs/Ogólne/Options/Audio/SFX/SFX_on".pressed))
+	save_file.set_value('FMOD Sound System Studio API', 'SPEAKERMODE', str($"tabs/Ogólne/Options/Audio/SPEAKERMODE".selected))
 #	save_file.set_value('Graphics', 'fullscreen', str($"tabs/Ogólne/Options/Graphics/Fullscreen".pressed))
 	save_file.set_value('Graphics', 'vsync_enabled', str($"tabs/Ogólne/Options/Graphics/VSync".pressed))
 	save_file.set_value('Graphics', 'vsync_via_compositor', str($"tabs/Ogólne/Options/Graphics/VSync".pressed))
@@ -46,6 +49,8 @@ func _process(_delta):
 func load_settings():
 	if file.file_exists('user://settings.cfg'):
 		save_file.load('user://settings.cfg')
+		if save_file.has_section_key('FMOD Sound System Studio API', 'SPEAKERMODE'):
+			$"tabs/Ogólne/Options/Audio/SPEAKERMODE".select(int(save_file.get_value("FMOD Sound System Studio API", "SPEAKERMODE", 1)))
 		if save_file.has_section_key('Audio', 'master_bus_volume'):
 			$"tabs/Ogólne/Options/Audio/Master/Master_slider".set_value(float(save_file.get_value('Audio', 'master_bus_volume', 0)))
 		if save_file.has_section_key('Audio', 'master_bus_enabled'):
@@ -287,3 +292,24 @@ func _on_InstallDLC_pressed():
 	if str(OS.get_name()) == "Android":
 		$Control.popup_centered()
 	OS.shell_open('https://www.sonadow-rpg.ml/dlcs/')
+
+
+func _on_SPEAKERMODE_item_selected(index):
+	if index == 0:#MONO
+		Fmod.set_software_format(0, Fmod.FMOD_SPEAKERMODE_MONO, 0)
+	if index == 1:#STEREO
+		Fmod.set_software_format(0, Fmod.FMOD_SPEAKERMODE_STEREO, 0)
+	if index == 2:#QUAD
+		Fmod.set_software_format(0, Fmod.FMOD_SPEAKERMODE_QUAD, 0)
+	if index == 3:#RAW
+		Fmod.set_software_format(0, Fmod.FMOD_SPEAKERMODE_RAW, 0)
+	if index == 4:#5.1
+		Fmod.set_software_format(0, Fmod.FMOD_SPEAKERMODE_5POINT1, 0)
+	if index == 5:#7.1
+		Fmod.set_software_format(0, Fmod.FMOD_SPEAKERMODE_7POINT1, 0)
+	if index == 6:#7.1.4
+		Fmod.set_software_format(0, Fmod.FMOD_SPEAKERMODE_7POINT1POINT4, 0)
+	if index == 7:#SURROUND
+		Fmod.set_software_format(0, Fmod.FMOD_SPEAKERMODE_SURROUND, 0)
+	if index == 8:#DEFAULT
+		Fmod.set_software_format(0, Fmod.FMOD_SPEAKERMODE_DEFAULT, 0)
